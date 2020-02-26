166be712b337aae6e7ee61ed704ce39f6f98e71c
Juan Carlos Torres <jutorres@insulet.com>
2020-02-25 22:25:55 -0800

    update constants

--- utils/constants.php
+++ utils/constants.php
@@ -1,10 +1,15 @@
 <?php
-class Constants{
+
 // define("DB_HOST_URL", "sql308.epizy.com");
-public const DB_HOST_URL = "sql308.epizy.com";
+$DB_HOST_URL    = "sql308.epizy.com";
 // define("DB_USER_NAME", "epiz_25016279");
-public const DB_USER_NAME = "epiz_25016279";
+$DB_USER_NAME   = "epiz_25016279";
 // define("DB_PASS", "B6eeqljrYeZ");
-public const DB_PASS = "B6eeqljrYeZ";
-}
+$DB_PASS        = "B6eeqljrYeZ";
+
+$DB_NAME        = "computacionEnServidor";
+$DSN            = "mysql:host=$DB_HOST_URL;dbname=$DB_USER_NAME";
+$options        = array(
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                    );
 ?>
